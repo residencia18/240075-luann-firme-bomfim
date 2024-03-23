1aa093e110e71294656397c6dfd4a505c5b008e0
Luann Firme <luann.firme@daten.com.br>
2024-03-23 09:54:23 -0300

    Exercicio 1 Aula 02

---
+++ Aula3-Exercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<double> originalList = new List<double> { 3, 7, 2, 4, 6 };
+
+        List<double> novaLista = originalList.ConvertAll(x => x / 2);
+
+        novaLista.ForEach(x => Console.WriteLine(x));
+    }
+}
