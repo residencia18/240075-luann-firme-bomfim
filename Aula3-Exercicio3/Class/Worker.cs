1d395134d2b8dcfbe30df0b38f17d4a953703cad
Luann Firme <luann.firme@daten.com.br>
2024-03-23 11:15:40 -0300

    Exercicio 3

---
+++ Aula3-Exercicio3/Class/Worker.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula3_Exercicio3;
+
+public class Worker
+{
+        public void Work()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Trabalhando... ({i}/5)");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Aula3-Exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aula3_Exercicio3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Worker worker1 = new Worker();
+        Worker worker2 = new Worker();
+
+        Thread thread1 = new Thread(worker1.Work);
+        Thread thread2 = new Thread(worker2.Work);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Ambas as threads terminaram o trabalho. Programa encerrado.");
+    }
+
+}
